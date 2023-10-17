3a0c55a948fb657da80245b0555c761ee7240f3d
Swarna Priya <85057078+Swarna-priya@users.noreply.github.com>
2023-10-17 13:16:48 +0530

    Add files via upload

---
+++ pangram.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class pangram {
+    public static void main(String args[]) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter Your String:");
+
+            String str = sc.nextLine();
+
+            str = str.replaceAll("", "").toLowerCase();
+
+            String s = "";
+
+            for (char i = 'a'; i <= 'z'; i++) {
+
+                if (str.indexOf(i) != -1) {
+
+                    s = s + i;
+                }
+            }
+
+            if (s.length() == 26) {
+                System.out.println("Pangram");
+            } else {
+                System.out.println(" Not Pangram");
+            }
+        }
+    }
+}
